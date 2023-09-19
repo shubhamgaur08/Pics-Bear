fa37ab98700759a3a3ed4eab42b14bd362325b48
shubham gaur <shubhamgaur672@gmail.com>
2023-09-19 20:49:16 +0530

    2nd

--- src/App.js
+++ src/App.js
@@ -6,21 +6,13 @@ import { getAnalytics } from "firebase/analytics";
 
 // Analytics
 const firebaseConfig = {
-
   apiKey: "AIzaSyA9y3ED6okc-WFmbojgkFGb3232QndpeLs",
-
   authDomain: "picsbear-c9487.firebaseapp.com",
-
   projectId: "picsbear-c9487",
-
   storageBucket: "picsbear-c9487.appspot.com",
-
   messagingSenderId: "807554734794",
-
   appId: "1:807554734794:web:9852f0efd784b8b7cfb233",
-
   measurementId: "G-9VZDX24F3J"
-
 };
 
 
